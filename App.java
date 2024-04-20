b47ec8ffcf48856c7fe05bb0861b29ff54b2e9d1
Diya Lohani <diyalohani2023@gmail.com>
2024-04-20 16:41:26 +0530

    updated App.java to add replay button

--- App.java
+++ App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        TicTacToe ticTacToe = new TicTacToe();
+        while (true) {
+            TicTacToe ticTacToe = new TicTacToe();
+            // The game is running until the user closes the window or chooses to quit
+            // If you want to replay automatically after the game ends, you can remove the
+            // break statement
+            if (!ticTacToe.gameOver) {
+                break; // Exit the loop if the game ends (window is closed or user chooses to quit)
+            }
+        }
     }
 }
